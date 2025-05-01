ff006a7a3a0fb365c2b617e7aab8b13aae4d88ff
for6to9si <for6to9si@gmail.com>
2025-05-01 21:08:44 +0700

    Fix service startup after reboot

--- etc/ndm/netfilter.d/000-swave.sh
+++ etc/ndm/netfilter.d/000-swave.sh
@@ -3,9 +3,8 @@
 logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
 logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
 
-[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit
+[ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit 0
 
 # $type is `iptables` or `ip6tables`
 # $table is `nat` or `mangle`
 /opt/etc/init.d/S99sing-box firewall_"$type"_"$table"
-
