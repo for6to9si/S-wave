bb4608d9126dd6990b9c544fd5ca2134e7a72b81
for6to9si <for6to9si@gmail.com>
2025-07-12 21:50:32 +0700

    Update 000-swave.sh

--- etc/ndm/netfilter.d/000-swave.sh
+++ etc/ndm/netfilter.d/000-swave.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
-logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
+#logger -p notice -t "$(basename "$0")" "Activate '${table}' routing tables"
+#logger -p notice -t "$(basename "$0")" "Activate '${type}' routing types"
 
 [ "$table" != "mangle" ] && [ "$table" != "nat" ] && exit 0
 
